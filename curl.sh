4fde3583a41b1fcd9a38cb5f7649d4958158d7dc
varungoud1112 <100950299+varungoud1112@users.noreply.github.com>
2023-05-21 17:41:47 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -8,34 +8,34 @@ NC='\033[0m'
 
 if [[ $OSTYPE == 'linux-android'* && $(echo "$TERMUX_VERSION" | cut -c 3-5) -ge "117" ]]; then
     dpkg -s git >>/dev/null 2>&1 || pkg install git -y
-    git clone https://github.com/varungoud1112/kkk || {
+    git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || {
         FILES='.tplaycreds .usercreds userDetails.json'
         for i in $FILES; do
-            cp -frp "kkk/$i" .
+            cp -frp "TataSky-Playlist-AutoUpdater/$i" .
         done
-        rm -rf kkk
-        git clone https://github.com/varungoud1112/kkk
+        rm -rf TataSky-Playlist-AutoUpdater
+        git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater
         for i in $FILES; do
-            cp "$i" "kkk/"
+            cp "$i" "TataSky-Playlist-AutoUpdater/"
         done
     }
-    cd kkk
+    cd TataSky-Playlist-AutoUpdater
     ./main.sh
 
 elif [[ $OSTYPE == 'linux-gnu'* ]]; then
     dpkg -s git >>/dev/null 2>&1 || sudo apt install git -y
-    git clone https://github.com/varungoud1112/kkk || {
+    git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || {
         FILES='.tplaycreds .usercreds userDetails.json'
         for i in $FILES; do
-            cp -frp "kkk/$i" .
+            cp -frp "TataSky-Playlist-AutoUpdater/$i" .
         done
-        rm -rf kkk
-        git clone https://github.com/varungoud1112/kkk
+        rm -rf TataSky-Playlist-AutoUpdater
+        git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater
         for i in $FILES; do
-            cp "$i" "kkk/"
+            cp "$i" "TataSky-Playlist-AutoUpdater/"
         done
     }
-    cd kkk
+    cd TataSky-Playlist-AutoUpdater
     ./main.sh
 elif [[ $(echo "$TERMUX_VERSION" | cut -c 3-5) -le "117" ]]; then
     echo -e "Please use Latest Termux release, i.e, from FDroid (https://f-droid.org/en/packages/com.termux/)"
